bda0e33deda92211c47955221103bbd5aba3d537
sandigul <sandigul@gmail.com>
2025-02-17 02:29:29 +0530

    implemented calling to llm

--- 2.ChatModels/1_chatmodel_openai.py
+++ 2.ChatModels/1_chatmodel_openai.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-model = ChatOpenAI(model='gpt-4' )
+model = ChatOpenAI(model='gpt-4', temperature=0, max_completion_tokens=100)
 result = model.invoke("What is the capital of India?")
 print(result)
